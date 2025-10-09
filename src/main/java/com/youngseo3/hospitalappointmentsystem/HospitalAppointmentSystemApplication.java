f938d9213ca866c13dea84911ac954ddcbbb95e4
youngseo3 <wys4475@gmail.com>
2025-10-09 19:17:53 +0900

    initial commit

---
+++ src/main/java/com/youngseo3/hospitalappointmentsystem/HospitalAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.youngseo3.hospitalappointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAppointmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youngseo3/hospitalappointmentsystem/HospitalAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngseo3.hospitalappointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
