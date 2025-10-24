a7361d3c4faee04123683798f001a2212b12a34d
youngseo3 <wys4475@gmail.com>
2025-10-24 13:58:42 +0900

    refactor: DTO에서 @Setter 제거 - MappingJackson2HttpMessageConverter가 ObjectMapper 클래스를 사용 - 내부적으로 리플렉션을 사용하여 필드명을 찾기 때문에 @Setter 불필요

--- src/main/java/com/youngseo3/hospitalappointmentsystem/dto/ReservationCreateRequest.java
+++ src/main/java/com/youngseo3/hospitalappointmentsystem/dto/ReservationCreateRequest.java
@@ -3,12 +3,10 @@ package com.youngseo3.hospitalappointmentsystem.dto;
 import com.youngseo3.hospitalappointmentsystem.entity.Reservation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
-@Setter
 @Getter
 public class ReservationCreateRequest {
     private Long patientId;
