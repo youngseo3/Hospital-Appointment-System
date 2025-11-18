5cb61a8937a7e95a39ac1f4dde3537c4ee9712fd
youngseo3 <wys4475@gmail.com>
2025-11-18 17:08:01 +0900

    refactor: RequestMapping URL 변경

--- src/main/java/com/youngseo3/hospitalappointmentsystem/controller/ReservationController.java
+++ src/main/java/com/youngseo3/hospitalappointmentsystem/controller/ReservationController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/reservations")
+@RequestMapping("/api/reservations")
 @RestController
 public class ReservationController {
     private final ReservationService reservationService;
